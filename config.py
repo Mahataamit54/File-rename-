a74590d7ba140166ddaf5866b79f470b270f60b6
amit_07 <154345897+Mahataamit54@users.noreply.github.com>
2025-05-21 19:09:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "461969be0f5af13fc50a55f6e783d6c8")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7488436463:AAGIHMc2F79zr_lQcDXscxqN2IPtNhxjiM8") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "MovieAMIT_bot") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "amit_movies") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
